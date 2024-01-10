bdb22636405756b151046347da067994fc350daf
Chris Rohlfs <seeken@gmail.com>
2024-01-09 22:42:12 -0700

    stub for save files

--- priv/templates/templates/save.migration.exs
+++ priv/templates/templates/save.migration.exs
@@ -1,15 +1,22 @@
 defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.table) %> do
-  use <%= inspect schema.migration_module %>
+  use Ecto.Migration
 
   def change do
-    create table(:<%= schema.table %><%= if schema.binary_id do %>, primary_key: false<% end %><%= if schema.prefix do %>, prefix: :<%= schema.prefix %><% end %>) do
-<%= if schema.binary_id do %>      add :id, :binary_id, primary_key: true
-<% end %><%= for {k, v} <- schema.attrs do %>      add <%= inspect k %>, <%= inspect Mix.Phoenix.Schema.type_for_migration(v) %><%= schema.migration_defaults[k] %>
-<% end %><%= for {_, i, _, s} <- schema.assocs do %>      add <%= inspect(i) %>, references(<%= inspect(s) %>, on_delete: :nothing<%= if schema.binary_id do %>, type: :binary_id<% end %>)
-<% end %>
+    create table(:saved_views) do
+      add :name, :string
+      add :context, :string
+      add :params, :map
+
       timestamps()
     end
-<%= if Enum.any?(schema.indexes) do %><%= for index <- schema.indexes do %>
-    <%= index %><% end %>
-<% end %>  end
+
+    create(
+      unique_index(
+      :saved_views,
+        ~w(name context)a,
+        name: :index_for_saved_view_name_context
+      )
+    )
+  end
+
 end
--- priv/templates/templates/save.schema.ex
+++ priv/templates/templates/save.schema.ex
@@ -1,4 +1,22 @@
 defmodule <%= inspect savecontext.schema %> do
 
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "saved_views" do
+    field :context, :string
+    field :name, :string
+    field :params, :map
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(saved_view, attrs) do
+    saved_view
+    |> cast(attrs, [:name, :context, :params])
+    |> validate_required([:name, :context, :params])
+    |> unique_constraint([:name, :context], name: :index_for_saved_view_name_context)
+  end
 
 end
